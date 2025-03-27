4e7254911b73cce8aee791a13f2957e79b306cdc
Ahmed ramadan <Ahmedr2304@gmail.com>
2025-03-27 18:06:15 +0300

    Creation the delegation function and how to call the delegation function

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+static void Welocme(string name) => Console.WriteLine("Welcome " + name);
+static void Hello(string name) => Console.WriteLine("Hello " + name);
+
+//MyDelegate d1 = new MyDelegate(Welocme);
+MyDelegate d1 = Welocme;
+d1("Ahmed");
+//MyDelegate d2 = new MyDelegate(Hello);
+MyDelegate d2 = Hello;
+d2("Mohammed");
+
+
+static void InvokeMyDelegate(MyDelegate myDelegate, string name) => myDelegate.Invoke(name);
+
+InvokeMyDelegate(d1, "Ahmed");
+InvokeMyDelegate(d2, "Mohamed");
+
+// Delegate Declaration
+public delegate void MyDelegate(string message);
+
+
+
+
+
+
+
+
+
+
+
+
+
