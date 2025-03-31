7eedd62e2f0ac10f54e9f93002ff7b7ed25fc48f
Ahmed ramadan <Ahmedr2304@gmail.com>
2025-03-31 19:01:06 +0300

    Add new class to define new delegate return bool and pass this delegation  as paramter to Count method that apply this condition

---
+++ Delegates/BookManagment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class BookManagment
+    {
+        public delegate bool Predicate(Book book);
+
+        
+        public static int CountBooks(List<Book> books, Predicate predicate) 
+        {
+            int count = 0;
+            foreach (Book book in books) 
+            {
+                if (predicate(book)) 
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string title, decimal price)
+        {
+            this.Title = title;
+            this.Price = price;
+
+        }
+
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,5 +1,34 @@
-﻿
+﻿using Delegates;
 
+#region Using delegation method as predicate paramter
+
+using Delegates;
+
+List<Book> books = new List<Book>()
+        {
+            new Book("The programmer" , 32.05M),
+            new Book("Clean Code" , 40.1M),
+            new Book("Fundementals of Algorithms" , 20.0M),
+            new Book("C# Modern" , 15.3M),
+            new Book("Head first Design Patterns" , 32M)
+        };
+
+bool GreaterThan30(Book book) => book.Price > 30M;
+bool LessThan20(Book book) => book.Price < 20M;
+
+bool StartWithC(Book book) => book.Title.StartsWith('C');
+
+
+//get the count of the books that are greater than 30
+Console.WriteLine("The count of the books that are greater than 30 : " + BookManagment.CountBooks(books, GreaterThan30));
+//get the count of the booksthat are less than 20
+Console.WriteLine("The count of the books that are Less than 20 : " + BookManagment.CountBooks(books, LessThan20));
+//get the count of the books that are starting with C
+Console.WriteLine("The count of the books that are starting with C : " + BookManagment.CountBooks(books, StartWithC));
+
+#endregion
+
+#region Using SimpleDelegation
 
 
 static void Welocme(string name) => Console.WriteLine("Welcome " + name);
@@ -20,6 +49,11 @@ InvokeMyDelegate(d2, "Mohamed");
 
 // Delegate Declaration
 public delegate void MyDelegate(string message);
+#endregion
+
+
+
+
 
 
 
